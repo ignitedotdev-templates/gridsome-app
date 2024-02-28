73cfd97a6c91e7f4ec6ebd60ca85770beeda7360
CharlesChinedum <charleschinedum2@gmail.com>
2024-02-28 01:30:16 +0100

    update: implemented UI

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="layout">
-    <header class="header">
+    <!-- <header class="header">
       <strong>
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
       </strong>
@@ -8,8 +8,8 @@
         <g-link class="nav__link" to="/">Home</g-link>
         <g-link class="nav__link" to="/about/">About</g-link>
       </nav>
-    </header>
-    <slot/>
+    </header> -->
+    <slot />
   </div>
 </template>
 
@@ -23,9 +23,10 @@ query {
 
 <style>
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 0;
+  padding: 0;
   line-height: 1.5;
 }
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,89 @@
 <template>
   <Layout>
-
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <main>
+      <div class="">
+        <g-image alt="Example image" src="~/favicon.png" width="135" />
+        <!-- <img src="{logo}" alt="logo" className="logo" /> -->
+      </div>
+      <div class="container">
+        <div class="inner-container">
+          <h2 class="header">
+            You’ve just ignited a new
+            <span class="library"> Gridsome </span> static site!
+          </h2>
+          <span class="pathname"> {{ currentPath }} </span>
+        </div>
+        <p class="footer">Made in Nigeria with 💖</p>
+      </div>
+    </main>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
-  }
-}
+    title: "Hello, world!",
+  },
+  data() {
+    return {
+      currentPath: "",
+    };
+  },
+  mounted() {
+    this.currentPath = window.location.href;
+  },
+};
 </script>
 
 <style>
 .home-links a {
   margin-right: 1rem;
+}
+
+main {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 7px;
+  align-items: center;
+  justify-content: center;
+}
+.logo {
+  width: 200px;
+  height: 200px;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  gap: 10px;
+}
+.inner-container {
+  display: flex;
+  flex-direction: column;
+  line-height: 1;
+}
+.header {
+  font-size: 36px;
+  font-weight: 400;
+  color: #344054;
+}
+.pathname {
+  color: #004eeb;
+  font-size: 20px;
+}
+.footer {
+  font-size: 20px;
+  font-weight: 500;
+  color: #344054;
+}
+.library {
+  color: #00a672;
+  font-weight: 600;
+  margin-left: 10px;
+  margin-right: 10px;
 }
 </style>
